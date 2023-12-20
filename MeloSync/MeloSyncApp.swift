500ed82473e709a41d5389a73f7a46dc6d0edcea
Santiago Rodriguez <svntiiago@gmail.com>
2023-12-20 14:07:51 -0500

    Initial Commit

---
+++ MeloSync/MeloSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeloSyncApp.swift
+//  MeloSync
+//
+//  Created by santiago on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MeloSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
